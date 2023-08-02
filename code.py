6bba9c57dea4a66f554fa81519266f97fb18e007
Praveensaich <97016531+Praveensaich@users.noreply.github.com>
2023-08-02 19:03:31 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,8 @@
 import streamlit
+import pandas as pd
 streamlit.title("Snowflake")
 streamlit.header("SQL")
 streamlit.text("Praveen sai ch")
 streamlit.text("Dabagardens, 30-1-29, Visakhapatnam")
+my_fruits_list = pd.read_csv(https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+streamlit.dataframe(my_fruit_list)
