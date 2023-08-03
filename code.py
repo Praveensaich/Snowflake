4e48e10d874cc17d808755921c0a391eaa58d4a4
Praveensaich <97016531+Praveensaich@users.noreply.github.com>
2023-08-03 15:14:26 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,8 @@
 import streamlit
-import pandas as pd
+import pandas
 streamlit.title("Snowflake")
 streamlit.header("SQL")
 streamlit.text("Praveen sai ch")
 streamlit.text("Dabagardens, 30-1-29, Visakhapatnam")
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
