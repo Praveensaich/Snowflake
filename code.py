521e45281c9fa875d33aa35b6fa3c26f56b21154
Praveensaich <97016531+Praveensaich@users.noreply.github.com>
2023-08-17 15:51:35 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,12 +1,3 @@
-import pandas
-import requests
-streamlit.title("Snowflake")
-streamlit.header("SQL")
-streamlit.text("Praveen sai ch")
-streamlit.text("Dabagardens, 30-1-29, Visakhapatnam")
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-streamlit.header("Fruityvice Fruit Advice!")
 import streamlit
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
