06253b3971a10666ccf113b7ccc51e42c434904a
Praveensaich <97016531+Praveensaich@users.noreply.github.com>
2023-08-17 15:53:59 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
