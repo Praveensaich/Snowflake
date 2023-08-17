d61e1a86153e0c6173a7d0147eb16c4392c4f427
Praveensaich <97016531+Praveensaich@users.noreply.github.com>
2023-08-17 15:49:46 +0530

    Update code.py

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ streamlit.text("Praveen sai ch")
 streamlit.text("Dabagardens, 30-1-29, Visakhapatnam")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
-fruitvyc_response = requests.get("https://fruityvice.com/api/fruit/watermelon");
-streamlit.text(fruitvyc_response);
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
